440c011907888467c69275f3f77f0256662abad6
diego-af <dyegoo.1295@gmail.com>
2025-03-29 17:46:55 -0300

    ✨ feat: Chnage Header and porvider router

--- src/RouteProvider.tsx
+++ src/RouteProvider.tsx
@@ -1,13 +1,18 @@
-import {createBrowserRouter} from 'react-router';
+import {createBrowserRouter, Navigate} from 'react-router';
 import {Login} from './pages/Login/Login';
 import {SignUp} from './pages/SignUp/SignUp';
 import {Gallery} from './pages/Gallery/Galerry';
 
 const router = createBrowserRouter([
 	{
+		path: '/',
+		element: <Navigate to='/auth' replace />
+	},
+	{
 		path: '/signup',
 		element: <SignUp />
 	},
+
 	{
 		path: '/auth',
 		element: <Login />
